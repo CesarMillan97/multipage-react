75aea0ced6a98595e0f3f0669109d53f90cce45e
Cesar Millan <ces_pain@hotmail.com>
2022-05-13 16:19:05 -0500

    Finished router links

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import { useLocation } from 'react-router-dom'
 
 export default function Contact() {
+  const queryString = useLocation().search
+  console.log(queryString);
+
+  const queryParams = new URLSearchParams(queryString)
+  const name = queryParams.get("name")
+  console.log(name);
+
   return (
    <div>
-      <h2>Contact</h2>
+      <h2>Hello {name}, Why don't you contact us?</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla quasi quae possimus officiis iure incidunt, doloribus provident libero voluptate nobis recusandae sint id ducimus deleniti ad accusamus, rem natus corrupti!</p>
    </div>
   )
